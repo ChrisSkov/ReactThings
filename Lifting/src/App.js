90cc829a42b6e59559083696fa3616f8d2c17f71
Chris <Stofferdyr@gmail.com>
2019-11-01 12:14:13 +0100

    todo excercise 1 done.

--- Lifting/src/App.js
+++ Lifting/src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import TodoList from "./TodoList";
+import NewTodo from "./NewTodo";
+import uuid from "uuid/v1";
 
 function App() {
+  const initialData = [
+    { id: uuid(), todoText: "Wake up" },
+    { id: uuid(), todoText: "Make Coffee" },
+    { id: uuid(), todoText: "Drink Coffee" }
+  ];
+  const [todos, setTodos] = useState(initialData);
+  const [newTodo, setNewTodo] = useState({ id: "", todoText: "" });
+  console.log(todos);
+
+  const addTodo = todo => {
+    if (todo.id === "") {
+      todo.id = uuid();
+      todos.push(todo);
+    } else {
+      let todoToEdit = todos.find(t => t.id === todo.id);
+      todoToEdit.todoText = todo.todoText;
+    }
+    setTodos([...todos]);
+    setNewTodo({ id: "", todoText: "" });
+  };
+ 
+  const deleteTodo = id => {
+
+    setTodos(
+      todos.filter(todo => {
+        return todo.id !== id;
+      })
+    );
+  };
+  const editTodo = index => {
+
+    const todoEdit = todos.find(element => {
+      return element.id === index;
+    });
+    setNewTodo(todoEdit);
+  };
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container outer">
+      <h2 style={{ textAlign: "center", marginBottom: 25 }}>
+        Props and Lifting State Demo
+      </h2>
+
+      <div className="row">
+        <div className="col-6 allTodos">
+          <TodoList todos={todos} deleteTodo={deleteTodo} editTodo={editTodo} />
+        </div>
+        <div className="col-5 new-todo">
+          <NewTodo addTodo={addTodo} nextTodo={newTodo} />
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
